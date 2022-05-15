74de7f4bfc226810fdc8598718fa23a7512412de
Chatter-Boo <psakhil211@gmail.com>
2022-05-15 21:53:53 +0530

    ignore

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-require('./db/connect')
 const  express = require('express')
-const app = express()
-const port = 3000
+
 const tasks = require('./routes/tasks.js')
+const connectDb = require('./db/connect')
 
 //middleware (to access data )
 
@@ -11,6 +10,15 @@ app.use(express.json())
 //routes
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-
+const app = express()
+const port = 3000
 app.use('/api/v1/tasks', tasks)
+
+const start = async()=>{
+try {
+    
+} catch (error) {
+    
+}
+
+}
--- db/connect.js
+++ db/connect.js
@@ -2,18 +2,22 @@ const mongoose = require("mongoose")
 
 const connectionString = 'mongodb+srv://akhil:akp7594959033@cluster0.7takx.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority'
 
+const connectDb = (url) => {
 
+    return mongoose
+        .connect(connectionString,
+            {
+                useNewUrlParser: true,
+                useCreateIndex: true,
 
-mongoose
-    .connect(connectionString,
-        {
-            useNewUrlParser: true,
-            useCreateIndex: true,
+                useFindAndModify: false,
 
-            useFindAndModify: false,
+                useUnifiedTopology: true
+            })
 
-            useUnifiedTopology: true
-        })
+}
 
-    .then(() => console.log('CONNECTED TO THE DB...'))
-    .catch((err) => console.log(err));
+module.exports = connectDb
+
+    // .then(() => console.log('CONNECTED TO THE DB...'))
+    // .catch((err) => console.log(err));
